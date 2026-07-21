d9bc72dc43e8a57f8777df01f041df02f6e53247
Daniel Yong <danielysj790@gmail.com>
2026-07-22 00:14:59 +0800

    fix: update MongoDB client configuration for improved TLS handling and add certifi dependency

--- backend/db/db_init.py
+++ backend/db/db_init.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import os
 import sys
+import certifi
 from dotenv import load_dotenv
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -9,13 +10,18 @@ load_dotenv()
 async def get_mongo_client():
     mongo_uri = os.getenv("MONGO_URI")
     database_name = os.getenv("MONGO_DB_NAME")
-    
+
     if not mongo_uri:
         raise ValueError("MONGO_URI environment variable is not set.")
-    
-    client = MongoClient(mongo_uri, tlsAllowInvalidCertificates=True)
-    _ = client.admin.command("ping")
-    
+
+    client = MongoClient(
+        mongo_uri,
+        tls=True,
+        tlsCAFile=certifi.where(),
+        serverSelectionTimeoutMS=20000,
+    )
+    _ = client.admin.command("ping")    
+
     db = client[database_name]
-    
+
     return db
